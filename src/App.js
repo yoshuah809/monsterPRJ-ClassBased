58b8e8e4ef48236b8f28ac244550cb8706ab4dc0
yoshuah809 <91335755+yoshuah809@users.noreply.github.com>
2022-04-12 13:06:54 -0500

    Added some styles to main landing page, gave fluent blue color

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import { Component } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
+import "./App.css";
 
 class App extends Component {
   constructor() {
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -4,7 +4,7 @@ class SearchBox extends Component {
   render() {
     const { onSearchChange } = this.props;
     return (
-      <div className="form-group  container mt-4 sm">
+      <div className="form-group  container my-4" style={{ width: "50%" }}>
         <input
           type="search"
           className="form-control"
